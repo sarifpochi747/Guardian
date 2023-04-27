7a366f178fe8014bfe7cbcfb8bdf03c7b2197b12
sarif pochi <sarifpochi747@gmail.com>
2023-04-27 13:38:23 +0700

    pagination

--- src/app/menu_page/user-page/user-page.component.ts
+++ src/app/menu_page/user-page/user-page.component.ts
@@ -1,10 +1,222 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-user-page',
   templateUrl: './user-page.component.html',
   styleUrls: ['./user-page.component.css']
 })
 export class UserPageComponent {
+
+  POST:any;
+  page:number = 1;
+  count:number = 0;
+  tableSize :number = 4;
+  
+  data:any = [
+                {
+                  comment: "ipsum dolor sit amet, adhuc nulla definiebas mei ad, ei doming aperiam delic...",
+                  company:"Savannah Miles",
+                  brandManager:"brand manager"
+                },
+                {
+                  comment: "ipsum dolor sit amet, adhuc nulla definiebas mei ad, ei doming aperiam delic...",
+                  company:"Savannah Miles",
+                  brandManager:"brand manager"
+                },
+                {
+                  comment: "ipsum dolor sit amet, adhuc nulla definiebas mei ad, ei doming aperiam delic...",
+                  company:"Savannah Miles",
+                  brandManager:"brand manager"
+                },
+                {
+                  comment: "ipsum dolor sit amet, adhuc nulla definiebas mei ad, ei doming aperiam delic...",
+                  company:"Savannah Miles",
+                  brandManager:"brand manager"
+                },
+                {
+                  comment: "ipsum dolor sit amet, adhuc nulla definiebas mei ad, ei doming aperiam delic...",
+                  company:"Savannah Miles",
+                  brandManager:"brand manager"
+                },
+                {
+                  comment: "ipsum dolor sit amet, adhuc nulla definiebas mei ad, ei doming aperiam delic...",
+                  company:"Savannah Miles",
+                  brandManager:"brand manager"
+                },
+                {
+                  comment: "ipsum dolor sit amet, adhuc nulla definiebas mei ad, ei doming aperiam delic...",
+                  company:"Savannah Miles",
+                  brandManager:"brand manager"
+                },
+                {
+                  comment: "ipsum dolor sit amet, adhuc nulla definiebas mei ad, ei doming aperiam delic...",
+                  company:"Savannah Miles",
+                  brandManager:"brand manager"
+                },
+                {
+                  comment: "ipsum dolor sit amet, adhuc nulla definiebas mei ad, ei doming aperiam delic...",
+                  company:"Savannah Miles",
+                  brandManager:"brand manager"
+                },
+                {
+                  comment: "ipsum dolor sit amet, adhuc nulla definiebas mei ad, ei doming aperiam delic...",
+                  company:"Savannah Miles",
+                  brandManager:"brand manager"
+                },
+                {
+                  comment: "ipsum dolor sit amet, adhuc nulla definiebas mei ad, ei doming aperiam delic...",
+                  company:"Savannah Miles",
+                  brandManager:"brand manager"
+                },
+                {
+                  comment: "ipsum dolor sit amet, adhuc nulla definiebas mei ad, ei doming aperiam delic...",
+                  company:"Savannah Miles",
+                  brandManager:"brand manager"
+                },
+                {
+                  comment: "ipsum dolor sit amet, adhuc nulla definiebas mei ad, ei doming aperiam delic...",
+                  company:"Savannah Miles",
+                  brandManager:"brand manager"
+                },
+                {
+                  comment: "ipsum dolor sit amet, adhuc nulla definiebas mei ad, ei doming aperiam delic...",
+                  company:"Savannah Miles",
+                  brandManager:"brand manager"
+                },
+                {
+                  comment: "ipsum dolor sit amet, adhuc nulla definiebas mei ad, ei doming aperiam delic...",
+                  company:"Savannah Miles",
+                  brandManager:"brand manager"
+                },
+                {
+                  comment: "ipsum dolor sit amet, adhuc nulla definiebas mei ad, ei doming aperiam delic...",
+                  company:"Savannah Miles",
+                  brandManager:"brand manager"
+                },
+                {
+                  comment: "ipsum dolor sit amet, adhuc nulla definiebas mei ad, ei doming aperiam delic...",
+                  company:"Savannah Miles",
+                  brandManager:"brand manager"
+                },
+                {
+                  comment: "ipsum dolor sit amet, adhuc nulla definiebas mei ad, ei doming aperiam delic...",
+                  company:"Savannah Miles",
+                  brandManager:"brand manager"
+                },
+                {
+                  comment: "ipsum dolor sit amet, adhuc nulla definiebas mei ad, ei doming aperiam delic...",
+                  company:"Savannah Miles",
+                  brandManager:"brand manager"
+                },
+                {
+                  comment: "ipsum dolor sit amet, adhuc nulla definiebas mei ad, ei doming aperiam delic...",
+                  company:"Savannah Miles",
+                  brandManager:"brand manager"
+                },
+                {
+                  comment: "ipsum dolor sit amet, adhuc nulla definiebas mei ad, ei doming aperiam delic...",
+                  company:"Savannah Miles",
+                  brandManager:"brand manager"
+                },
+                {
+                  comment: "ipsum dolor sit amet, adhuc nulla definiebas mei ad, ei doming aperiam delic...",
+                  company:"Savannah Miles",
+                  brandManager:"brand manager"
+                },
+                {
+                  comment: "ipsum dolor sit amet, adhuc nulla definiebas mei ad, ei doming aperiam delic...",
+                  company:"Savannah Miles",
+                  brandManager:"brand manager"
+                },
+                {
+                  comment: "ipsum dolor sit amet, adhuc nulla definiebas mei ad, ei doming aperiam delic...",
+                  company:"Savannah Miles",
+                  brandManager:"brand manager"
+                },
+                {
+                  comment: "ipsum dolor sit amet, adhuc nulla definiebas mei ad, ei doming aperiam delic...",
+                  company:"Savannah Miles",
+                  brandManager:"brand manager"
+                },
+                {
+                  comment: "ipsum dolor sit amet, adhuc nulla definiebas mei ad, ei doming aperiam delic...",
+                  company:"Savannah Miles",
+                  brandManager:"brand manager"
+                },
+                {
+                  comment: "ipsum dolor sit amet, adhuc nulla definiebas mei ad, ei doming aperiam delic...",
+                  company:"Savannah Miles",
+                  brandManager:"brand manager"
+                },
+                {
+                  comment: "ipsum dolor sit amet, adhuc nulla definiebas mei ad, ei doming aperiam delic...",
+                  company:"Savannah Miles",
+                  brandManager:"brand manager"
+                },
+                {
+                  comment: "ipsum dolor sit amet, adhuc nulla definiebas mei ad, ei doming aperiam delic...",
+                  company:"Savannah Miles",
+                  brandManager:"brand manager"
+                },
+                {
+                  comment: "ipsum dolor sit amet, adhuc nulla definiebas mei ad, ei doming aperiam delic...",
+                  company:"Savannah Miles",
+                  brandManager:"brand manager"
+                },
+                {
+                  comment: "ipsum dolor sit amet, adhuc nulla definiebas mei ad, ei doming aperiam delic...",
+                  company:"Savannah Miles",
+                  brandManager:"brand manager"
+                },
+                {
+                  comment: "ipsum dolor sit amet, adhuc nulla definiebas mei ad, ei doming aperiam delic...",
+                  company:"Savannah Miles",
+                  brandManager:"brand manager"
+                },
+                {
+                  comment: "ipsum dolor sit amet, adhuc nulla definiebas mei ad, ei doming aperiam delic...",
+                  company:"Savannah Miles",
+                  brandManager:"brand manager"
+                },
+                {
+                  comment: "ipsum dolor sit amet, adhuc nulla definiebas mei ad, ei doming aperiam delic...",
+                  company:"Savannah Miles",
+                  brandManager:"brand manager"
+                },
+                {
+                  comment: "ipsum dolor sit amet, adhuc nulla definiebas mei ad, ei doming aperiam delic...",
+                  company:"Savannah Miles",
+                  brandManager:"brand manager"
+                },
+                {
+                  comment: "ipsum dolor sit amet, adhuc nulla definiebas mei ad, ei doming aperiam delic...",
+                  company:"Savannah Miles",
+                  brandManager:"brand manager"
+                },
+                {
+                  comment: "ipsum dolor sit amet, adhuc nulla definiebas mei ad, ei doming aperiam delic...",
+                  company:"Savannah Miles",
+                  brandManager:"brand manager"
+                },
+                {
+                  comment: "ipsum dolor sit amet, adhuc nulla definiebas mei ad, ei doming aperiam delic...",
+                  company:"Savannah Miles",
+                  brandManager:"brand manager"
+                },
+                {
+                  comment: "ipsum dolor sit amet, adhuc nulla definiebas mei ad, ei doming aperiam delic...",
+                  company:"Savannah Miles",
+                  brandManager:"brand manager"
+                },
+                {
+                  comment: "ipsum dolor sit amet, adhuc nulla definiebas mei ad, ei doming aperiam delic...",
+                  company:"Savannah Miles",
+                  brandManager:"brand manager"
+                },
+  ];
+
+  onTableDataChange(event : any)
+  {
+    this.page = event
+  }
 
 }
