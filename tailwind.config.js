a1728d545dcd99960f1a8c234741dd06f80e75d0
sarif pochi <sarifpochi747@gmail.com>
2023-05-05 21:48:04 +0700

    responsive user page

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,13 @@ theme: {
     Anuphan: ['Anuphan'],
     Poppins:['Poppins']
   },
+  screens: {
+    sm: "640px",
+    md: "769px",
+    lg: "1024px",
+    xl: "1280px",
+    "2xl": "1536px",
+  }
 },
 plugins: [],
 }
